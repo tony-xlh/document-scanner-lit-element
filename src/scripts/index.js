5a2d8fd956e4eeb6142d8b5e0564a0289fa1a469
xulihang <xulihanghai@163.com>
2023-11-03 10:50:39 +0800

    add scan and save buttons in index

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,35 @@
 import '../styles/index.scss';
-import { MyTimer } from './timer';
-import { DocumentScanner } from './documentscanner';
-console.log('webpack starterkit');
-console.log(MyTimer);
-console.log(DocumentScanner);
+import { DocumentScanner } from './documentscanner'; // eslint-disable-line
+let DWObject;
+window.onload = function(){
+  let scanner = document.querySelector("document-scanner");
+  scanner.addEventListener("initialized",function(e){
+    console.log(e);
+    DWObject = e.detail.DWObject;
+  });
+  document.getElementsByClassName("scan-button")[0].addEventListener("click",function(){
+    scan();
+  });
+  document.getElementsByClassName("save-button")[0].addEventListener("click",function(){
+    save();
+  });
+};
+
+function scan(){
+  if (DWObject) {
+    DWObject.SelectSource(function () {
+      DWObject.OpenSource();
+      DWObject.AcquireImage();
+    },
+      function () {
+        console.log("SelectSource failed!");
+      }
+    );
+  }
+}
+
+function save(){
+  if (DWObject) {
+    DWObject.SaveAllAsPDF("Scannd.pdf");
+  }
+}
