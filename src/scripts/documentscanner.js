9bc68a09206b4c91a88324e1f4d98ba02b74b454
xulihang <xulihanghai@163.com>
2023-11-03 15:21:24 +0800

    move buttons into the document scanner component

--- src/scripts/documentscanner.js
+++ src/scripts/documentscanner.js
@@ -21,37 +21,58 @@ export class DocumentScanner extends LitElement {
   }
 
   render() {
-    return html`<div id="dwtcontrolContainer"></div>`;
+    return html`
+    <div>
+      <button @click=${this.scan}>Scan</button>
+      <button @click=${this.save}>Save</button>
+    </div>
+    <div id="dwtcontrolContainer"></div>`;
+  }
+
+  scan(){
+    let pThis = this;
+    if (pThis.DWObject) {
+      pThis.DWObject.SelectSource(function () {
+        pThis.DWObject.OpenSource();
+        pThis.DWObject.AcquireImage();
+      },
+        function () {
+          console.log("SelectSource failed!");
+        }
+      );
+    }
+  }
+  
+  save(){
+    if (this.DWObject) {
+      this.DWObject.SaveAllAsPDF("Scannd.pdf");
+    }
   }
 
   firstUpdated() {
     let pThis = this;
     let dwtContainer = this.renderRoot.getElementById("dwtcontrolContainer");
-    Dynamsoft.DWT.Containers = [{ ContainerId: 'dwtcontrolContainer',Width: 270, Height: 350 }];
-    Dynamsoft.DWT.RegisterEvent('OnWebTwainReady', function () {
-      Dynamsoft.DWT.CreateDWTObjectEx(
-        {
-          WebTwainId: 'dwtcontrol'
-        },
-        function(obj) {
-          pThis.DWObject = obj;
-          pThis.DWObject.Viewer.bind(dwtContainer);
-          pThis.DWObject.Viewer.show();
-          pThis.DWObject.Viewer.width = "100%";
-          pThis.DWObject.Viewer.height = "100%";
-          const event = new CustomEvent('initialized', {
-            detail: {
-              DWObject: pThis.DWObject
-            }
-          });
-          pThis.dispatchEvent(event);
-        },
-        function(err) {
-          console.log(err);
-        }
-      );
-    });
-    Dynamsoft.DWT.Load();
+    Dynamsoft.DWT.CreateDWTObjectEx(
+      {
+        WebTwainId: 'dwtcontrol'
+      },
+      function(obj) {
+        pThis.DWObject = obj;
+        pThis.DWObject.Viewer.bind(dwtContainer);
+        pThis.DWObject.Viewer.show();
+        pThis.DWObject.Viewer.width = "100%";
+        pThis.DWObject.Viewer.height = "100%";
+        const event = new CustomEvent('initialized', {
+          detail: {
+            DWObject: pThis.DWObject
+          }
+        });
+        pThis.dispatchEvent(event);
+      },
+      function(err) {
+        console.log(err);
+      }
+    );
   } 
 }
 customElements.define('document-scanner', DocumentScanner);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,32 +4,8 @@ let DWObject;
 window.onload = function(){
   let scanner = document.querySelector("document-scanner");
   scanner.addEventListener("initialized",function(e){
-    console.log(e);
     DWObject = e.detail.DWObject;
-  });
-  document.getElementsByClassName("scan-button")[0].addEventListener("click",function(){
-    scan();
-  });
-  document.getElementsByClassName("save-button")[0].addEventListener("click",function(){
-    save();
+    console.log(DWObject);
   });
 };
 
-function scan(){
-  if (DWObject) {
-    DWObject.SelectSource(function () {
-      DWObject.OpenSource();
-      DWObject.AcquireImage();
-    },
-      function () {
-        console.log("SelectSource failed!");
-      }
-    );
-  }
-}
-
-function save(){
-  if (DWObject) {
-    DWObject.SaveAllAsPDF("Scannd.pdf");
-  }
-}
